f5a8c22bbde9679b1d720f30680e15f650c3e00a
Sam <samrandall100@gmail.com>
2022-09-07 19:41:13 -0500

    first commit

--- max_subarray.py
+++ max_subarray.py
@@ -2,12 +2,13 @@ from tkinter import N
 
 
 arr = [-1, 4, 2, -1, 3, 5]
+
 current_max = 0
-global_max = 0
-for number in range(0, len(arr)):
-    current_max = arr[number] + current_max
-    if(current_max < arr[number]):
-        current_max = arr[number]
+global_max = arr[0]
+for number in arr:
+    current_max = number + current_max
+    if current_max < number:
+        current_max = number
     if global_max < current_max:
         global_max = current_max
 
