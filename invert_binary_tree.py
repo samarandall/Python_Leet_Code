e5c6e3bedfcf68fbf2c6ff7f69a4415a533295c3
Sam <samrandall100@gmail.com>
2022-09-17 13:41:42 -0500

    added inver binary tree

--- invert_binary_tree.py
+++ invert_binary_tree.py
@@ -1,7 +1,39 @@
 class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+    def __init__(self, node_val):
+        self.node_val = node_val
+        self.left = None
+        self.right = None
         
-root = TreeNode()
+def invertBinaryTree(root):
+    if not root:
+        return None
+        
+    left = invertBinaryTree(root.left)
+    right = invertBinaryTree(root.right)
+    
+    root.left = right
+    root.right = left
+    return root
+
+def printTree(root):
+    if not root:
+        return None
+    left = printTree(root.left)
+    right = printTree(root.right)
+    
+    print(root.node_val)
+    return root
+    
+
+root = TreeNode(6)
+root.left = TreeNode(2)
+root.left.left = TreeNode(0)
+root.left.right = TreeNode(4)
+root.left.right.left = TreeNode(3)
+root.left.right.right = TreeNode(5)
+root.right = TreeNode(8)
+root.right.left = TreeNode(7)
+root.right.right = TreeNode(9)
+
+printTree(root)
+printTree(invertBinaryTree(root))
